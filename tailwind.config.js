a7b0402f443099801f0f2e273ce8a0a21735fbdd
Muhammad Faiq Yusuf Raharja <113581726+faiqyr@users.noreply.github.com>
2024-03-31 21:05:31 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '15px',
+
+    },
+    
+    extend: {
+      colors:{
+        primary: '#fafaf9',
+        orange: '#ea580c'
+      },
+     screens:{
+      '2xl': '1320px',
+     },
+    
+    },
+  },
+  plugins: [],
+}
+
